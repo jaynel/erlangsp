ac5426e0fc00f0fee870074e6bda869b9bbe8dfa
Jay Nelson <jay@duomark.com>
2014-04-20 14:56:55 -0700

    Move erlangsp app modules to a top-level src directory

--- src/erlangsp_app.erl
+++ src/erlangsp_app.erl
@@ -1,5 +1,5 @@
 %%%------------------------------------------------------------------------------
-%%% @copyright (c) 2012, DuoMark International, Inc.  All rights reserved
+%%% @copyright (c) 2012-2014, DuoMark International, Inc.  All rights reserved
 %%% @author Jay Nelson <jay@duomark.com>
 %%% @reference The license is based on the template for Modified BSD from
 %%%   <a href="http://opensource.org/licenses/BSD-3-Clause">OSI</a>
@@ -22,9 +22,9 @@
 %% Application callbacks
 %% ===================================================================
 
--spec start() -> {ok, pid()}.
--spec start(any(), any()) -> {ok, pid()}.
--spec stop([]) -> ok.
+-spec start ()             -> {ok, pid()}.
+-spec start (any(), any()) -> {ok, pid()}.
+-spec stop  ([])           -> ok.
 
 %% @doc Start the application's root supervisor in erl listener.
 start() ->
